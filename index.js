a2f8b5692543c361dfea6aa3214aab905dcd8702
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-23 07:40:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
 var name = "Tyrell"
-var height = "73"
+var height = 73
 
 console.log("Name:")
 console.log(name)
 
-console.log(parseInt(height) +1)
+console.log(height +1)
